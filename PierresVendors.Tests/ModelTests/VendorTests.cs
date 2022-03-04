a06ebd20ea677feb50dd0fa9e8fbfb59d1722560
Mikah Mathews <mikah@outofband.org>
2022-03-04 09:38:39 -0800

    Write test for getting name

--- PierresVendors.Tests/ModelTests/VendorTests.cs
+++ PierresVendors.Tests/ModelTests/VendorTests.cs
@@ -11,8 +11,17 @@ namespace PierresVendors.TestTools
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor testVendor = new Vendor("Pete's Coffee", "Puts in order every week");
+      Vendor testVendor = new Vendor("Pete's Coffee", "description");
       Assert.AreEqual(typeof(Vendor), testVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsString_String()
+    {
+      string testVendorName = "Pete's Coffee";
+      Vendor testVendor = new Vendor(testVendorName, "description");
+      string result = testVendor.Name;
+      Assert.AreEqual(testVendorName, result);
     }
   }
 }
