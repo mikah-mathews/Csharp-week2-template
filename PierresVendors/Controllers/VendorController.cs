d92559c47827ec8215b053bb0204f4be688054bf
Mikah Mathews <mikah@outofband.org>
2022-03-04 13:42:36 -0800

    Add vendor/order controllers, as well as vendor and order views

---
+++ PierresVendors/Controllers/VendorController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PierresVendors.Models;
+
+namespace PierresVendors.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription)
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      
+    }
+  }
+}
