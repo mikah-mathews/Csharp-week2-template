3df1bcef86b904ca4ce754883bd4312fab269bd4
Mikah Mathews <mikah@outofband.org>
2022-03-04 09:00:51 -0800

    Add Order and Vendor Models - start writing tests

---
+++ PierresVendors.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresVendors.TestTools
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    
+  }
+}
