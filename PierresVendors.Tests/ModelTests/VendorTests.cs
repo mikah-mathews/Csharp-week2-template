e1865d49bc63a5cc3f15ec002a0429c3a35d10df
Mikah Mathews <mikah@outofband.org>
2022-03-04 09:30:01 -0800

    Write first test to test constructor of Vendor

--- PierresVendors.Tests/ModelTests/VendorTests.cs
+++ PierresVendors.Tests/ModelTests/VendorTests.cs
@@ -1,13 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Models;
+using PierresVendors.Models;
 using System.Collections.Generic;
 using System;
 
 namespace PierresVendors.TestTools
 {
   [TestClass]
-  public class VendorTests : IDisposable
+  public class VendorTests 
   {
-    
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor testVendor = new Vendor("Pete's Coffee", "Puts in order every week");
+      Assert.AreEqual(typeof(Vendor), testVendor.GetType());
+    }
   }
 }
